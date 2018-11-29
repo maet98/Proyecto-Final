29aceaf1b9ab80bb30edebf940da446e3c39cbbd
malet <malet@DESKTOP-63K6I31>
2018-11-29 15:22:17 -0400

    Cambios en RegEquipo

--- LNB/src/visual/ControladorPartidos.java
+++ LNB/src/visual/ControladorPartidos.java
@@ -1,17 +1,21 @@
 package visual;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.plaf.BorderUIResource.TitledBorderUIResource;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JTable;
@@ -22,6 +26,12 @@ import logico.Equipo;
 import logico.Jugador;
 
 import javax.swing.ListSelectionModel;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ControladorPartidos extends JDialog {
 
@@ -34,8 +44,8 @@ public class ControladorPartidos extends JDialog {
 	private String[] columnNames = {"Numero","Nombre","Tiros Libres","Tiros de Dos","Tiros de Tres"};
 	private Equipo Local;
 	private Equipo Visitante;
-	private ArrayList<Desempenno> DesLocal;
-	private ArrayList<Desempenno> DesVisitante;
+	private int TanteoLocal;
+	private int TanteoVisitante;
 	
 	public ControladorPartidos(Equipo Local, Equipo Visitante) {
 		setTitle("Controlador Partido");
@@ -66,8 +76,6 @@ public class ControladorPartidos extends JDialog {
 			
 			modelLocal = new DefaultTableModel();
 			modelVisitante = new DefaultTableModel();
-			DesLocal = new ArrayList<>();
-			DesVisitante = new ArrayList<>();
 			JLabel lblEquipoLocal = new JLabel("Equipo Local");
 			lblEquipoLocal.setBounds(160, 13, 84, 16);
 			panel.add(lblEquipoLocal);
@@ -81,6 +89,28 @@ public class ControladorPartidos extends JDialog {
 			panel.add(scrollPaneLocal);
 			
 			tablaLocal = new JTable();
+			tablaLocal.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent event) {
+					int selectedIndex = tablaLocal.getSelectedRow();
+					if(selectedIndex>=0) {
+						System.out.println(event.getClickCount());
+						TanteoLocal = calcLocal();
+						cambiarMarcardor();
+					}
+				}
+			});
+			tablaLocal.addKeyListener(new KeyAdapter() {
+				public void keyPressed(KeyEvent event) {
+					if(Character.isDigit(event.getKeyChar())){
+						TanteoLocal = calcLocal();
+						System.out.println(TanteoLocal+" "+TanteoVisitante);
+						cambiarMarcardor();
+					}
+					else {
+						event.consume();
+					}
+				}
+			});
 			tablaLocal.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 			modelLocal.setColumnIdentifiers(columnNames);
 			tablaLocal.setModel(modelLocal);
@@ -91,10 +121,40 @@ public class ControladorPartidos extends JDialog {
 			panel.add(scrollPaneVisitante);
 			
 			Tablavisitante = new JTable();
+			Tablavisitante.addKeyListener(new KeyAdapter() {
+				public void keyTyped(KeyEvent e) {
+					if(Character.isDigit(e.getKeyChar())){
+						TanteoVisitante = calcVisitante();
+						cambiarMarcardor();
+					}
+					else {
+						e.consume();
+					}
+				}
+			});
 			Tablavisitante.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 			modelVisitante.setColumnIdentifiers(columnNames);
 			Tablavisitante.setModel(modelVisitante);
 			scrollPaneVisitante.setViewportView(Tablavisitante);
+			
+			JButton btnInsertarTiros = new JButton("Insertar Tiros");
+			btnInsertarTiros.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					int selectedRow = tablaLocal.getSelectedRow();
+					if(selectedRow>=0) {
+						int tirosLibres = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad de tiros libres", "Tiros libres", JOptionPane.INFORMATION_MESSAGE));
+						int tiroDe2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad de tiros de 2", "Tiros de 2", JOptionPane.INFORMATION_MESSAGE));
+						int tiroDe3 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad de tiros de 3", "Tiros de 3", JOptionPane.INFORMATION_MESSAGE));
+						modelLocal.setValueAt(tirosLibres, selectedRow, 2);
+						modelLocal.setValueAt(tiroDe2, selectedRow, 3);
+						modelLocal.setValueAt(tiroDe3, selectedRow, 4);
+						TanteoLocal = calcLocal();
+						cambiarMarcardor();
+					}
+				}
+			});
+			btnInsertarTiros.setBounds(147, 426, 120, 25);
+			panel.add(btnInsertarTiros);
 		}
 		{
 			JPanel buttonPane = new JPanel();
@@ -109,6 +169,31 @@ public class ControladorPartidos extends JDialog {
 		}
 		loadLocal();
 		loadVisitante();
+		calcLocal();
+		calcVisitante();
+		cambiarMarcardor();
+	}
+	private int calcLocal() {
+		int t = 0;
+		for(int i = 0;i < Local.getJugadores().size();i++) {
+			t += Integer.parseInt(tablaLocal.getValueAt(i, 2).toString());
+			t += Integer.parseInt(tablaLocal.getValueAt(i, 3).toString())*2;
+			t += Integer.parseInt(tablaLocal.getValueAt(i, 4).toString())*3;
+		}
+		return t;
+	}
+	private int calcVisitante() {
+		int t = 0;
+		for(int i = 0;i < Visitante.getJugadores().size();i++) {
+			t += Integer.parseInt(Tablavisitante.getValueAt(i, 2).toString());
+			t += Integer.parseInt(Tablavisitante.getValueAt(i, 3).toString())*2;
+			t += Integer.parseInt(Tablavisitante.getValueAt(i, 4).toString())*3;
+		}
+		return t;
+	}
+	private void cambiarMarcardor() {
+		String marcardor = TanteoLocal+" - "+TanteoVisitante;
+		txtMarcador.setText(marcardor);
 	}
 	private void loadLocal() {
 		Object[] fila = new Object[modelLocal.getColumnCount()];
@@ -135,3 +220,6 @@ public class ControladorPartidos extends JDialog {
 		}
 	}
 }
+
+
+
