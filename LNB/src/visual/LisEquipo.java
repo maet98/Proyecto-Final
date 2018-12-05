89664ca3d2854b173647b437850ac311f4de968e
Eduardo <Eduardo@10.0.0.6>
2018-12-05 19:24:13 -0400

    Listar Equipo

--- LNB/src/visual/LisEquipo.java
+++ LNB/src/visual/LisEquipo.java
@@ -2,13 +2,16 @@ package visual;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
@@ -23,33 +26,47 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JTextField;
 
 public class LisEquipo extends JDialog {
 	private JTable table;
 	private DefaultTableModel model;
 	private String selected = "";
 	private JButton btnModificar;
+	private JTextField txtNombre;
+	private JTextField txtEntrenador;
+	private JTextField txtEstadio;
+	private JTextField txtCiudad;
+	private JLabel lblLogo;
 
 
 	public LisEquipo() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(LisEquipo.class.getResource("/imagenes/basketball.png")));
 		setTitle("Listar Equipos");
-		setBounds(100, 100, 580, 377);
+		setBounds(100, 100, 680, 377);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
 		{
 			JPanel buttonPane = new JPanel();
-			buttonPane.setBounds(0, 305, 564, 33);
+			buttonPane.setBounds(6, 311, 668, 33);
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane);
 			{
 				btnModificar = new JButton("Modificar");
 				btnModificar.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						Equipo cambiar = Liga.getInstance().buscarEquipo(selected);
-						RegEquipo modificar = new RegEquipo(cambiar);
-						modificar.setModal(true);
-						modificar.setVisible(true);
+						
+						if(txtCiudad.getText().equals("") || txtEntrenador.getText().equals("") || txtEstadio.getText().equals("")) {
+							JOptionPane.showMessageDialog(null, "Debe llenar todos los campos", "Aviso", JOptionPane.WARNING_MESSAGE);
+						}else {
+							Equipo cambiar = Liga.getInstance().buscarEquipo(selected);
+							cambiar.setCiudad(txtCiudad.getText());
+							cambiar.setEntrenador(txtEntrenador.getText());
+							cambiar.setEstadio(txtEstadio.getText());
+							JOptionPane.showMessageDialog(null, "El equipo "+txtNombre.getText()+" ha sido modificado", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+							loadEquipos();
+						}
+						
 					}
 				});
 				btnModificar.setActionCommand("OK");
@@ -70,7 +87,7 @@ public class LisEquipo extends JDialog {
 		}
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 0, 564, 305);
+		scrollPane.setBounds(6, 150, 668, 154);
 		getContentPane().add(scrollPane);
 		
 		table = new JTable();
@@ -80,6 +97,12 @@ public class LisEquipo extends JDialog {
 				int selectedRow = table.getSelectedRow();
 				if(selectedRow>=0) {
 					selected = table.getValueAt(selectedRow, 0).toString();
+					txtNombre.setText(Liga.getInstance().buscarEquipo(selected).getNombre());
+					txtCiudad.setText(Liga.getInstance().buscarEquipo(selected).getCiudad());
+					txtEntrenador.setText(Liga.getInstance().buscarEquipo(selected).getEntrenador());
+					txtEstadio.setText(Liga.getInstance().buscarEquipo(selected).getEstadio());
+					ImageIcon imageicon = new ImageIcon(Liga.getInstance().buscarEquipo(selected).getLogo().getImage().getScaledInstance(lblLogo.getWidth(), lblLogo.getHeight(), Image.SCALE_DEFAULT));
+					lblLogo.setIcon(imageicon);
 					btnModificar.setEnabled(true);
 				}
 			}
@@ -91,6 +114,47 @@ public class LisEquipo extends JDialog {
 		model = new DefaultTableModel();
 		model.setColumnIdentifiers(columnNames);
 		table.setModel(model);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(6, 11, 61, 16);
+		getContentPane().add(lblNombre);
+		
+		txtNombre = new JTextField();
+		txtNombre.setEditable(false);
+		txtNombre.setBounds(80, 6, 230, 26);
+		getContentPane().add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JLabel lblEntrenador = new JLabel("Entrenador:");
+		lblEntrenador.setBounds(6, 44, 82, 16);
+		getContentPane().add(lblEntrenador);
+		
+		JLabel lblEstadio = new JLabel("Estadio:");
+		lblEstadio.setBounds(6, 81, 61, 16);
+		getContentPane().add(lblEstadio);
+		
+		JLabel lblCiudad = new JLabel("Ciudad:");
+		lblCiudad.setBounds(6, 114, 61, 16);
+		getContentPane().add(lblCiudad);
+		
+		lblLogo = new JLabel("");
+		lblLogo.setBounds(511, 25, 113, 113);
+		getContentPane().add(lblLogo);
+		
+		txtEntrenador = new JTextField();
+		txtEntrenador.setBounds(80, 39, 230, 26);
+		getContentPane().add(txtEntrenador);
+		txtEntrenador.setColumns(10);
+		
+		txtEstadio = new JTextField();
+		txtEstadio.setBounds(80, 76, 230, 26);
+		getContentPane().add(txtEstadio);
+		txtEstadio.setColumns(10);
+		
+		txtCiudad = new JTextField();
+		txtCiudad.setBounds(80, 109, 230, 26);
+		getContentPane().add(txtCiudad);
+		txtCiudad.setColumns(10);
 		loadEquipos();
 	}
 	
